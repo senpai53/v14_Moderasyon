564df18e391fab4a3a513735b7bd061b5a8a8811
senpai53 <143003142+senpai53@users.noreply.github.com>
2024-02-25 21:31:42 +0300

    Update oto-cevap.js

--- commands/oto-cevap.js
+++ commands/oto-cevap.js
@@ -8,13 +8,13 @@ module.exports = {
   options: [
     {
         name: "soru",
-        description: "Lütfen bir soru girin.",
+        description: "miloş",
         type: 3,
         required: true
     },
     {
       name: "cevap",
-      description: "Lütfen bir cevap girin.",
+      description: "sunucunun en ponçiği",
       type: 3,
       required: true
   },
